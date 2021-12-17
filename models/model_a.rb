0f2e4e1500b564fb61173f1322b30032f278223d
Phillip Kamps <phillip.kamps@gmail.com>
2021-12-17 13:14:18 -0700

    Add model_a

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  'name' => 'Drednaughtus',
+  'subgroup' => 'Titanosaurs',
+  'length_in_meters' => 25.9,
+  'weight_in_tonnes' => 59,
+  'is_herbivore' => true,
+  'has_predators' => false,
+  'diet' => ['leaves', 'grasses', 'shrubs'],
+  'defenses' => ['tail', 'size', 'weight']
+}
