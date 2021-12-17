3b4684ffc2a90085f5332a37b0215384724056e7
Phillip Kamps <phillip.kamps@gmail.com>
2021-12-17 12:56:46 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,17 @@
+movie = "Oblivion"
+
+is_entertaining = true
+
+movie = "Step Brothers"
+
+num_4 = 2
+
+num_6 = 87
+
+product = num_4 * num_6
+
+favorite_foods = ["greens", "tacos", "apples"]
+
+favorite_foods.delete_at(0)
+
+puts movie.length
