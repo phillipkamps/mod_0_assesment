e29979defc7710db55cddb06b5241e7c9ab4391f
Phillip Kamps <phillip.kamps@gmail.com>
2021-12-17 13:20:42 -0700

    Add model_b

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  'name' => 'Phillip',
+  'role' => 'backend',
+  'monitor_count' => 2,
+  'age' => 30,
+  'enrolled' => true,
+  'graduated' => false,
+  'courses_completed' => ['trycoding', 'mod0', 'mod1 prework'],
+  'courses_incomplete' => ['mod1', 'mod2', 'mod3', 'mod4']
+}
